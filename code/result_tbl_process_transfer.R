35195de5b2126a9c3918e83e3d34c0e09f5f6fce
karaesmen <ezgi.karaesmen@gmail.com>
2020-01-06 16:37:36 -0500

    fix integer issue for results table

--- code/result_tbl_process_transfer.R
+++ code/result_tbl_process_transfer.R
@@ -3,19 +3,21 @@ library(vroom)
 library(batch)
 batch::parseCommandArgs(evaluate=TRUE)
 
-# path_in <- "/projects/rpci/lsuchest/lsuchest/DBMT_results/DBMT_mixed/analyses/mixed_EA_results/out"
-# path_out <- "/projects/rpci/lsuchest/lsuchest/DBMT_results/DBprep/out"
-# patt <- "2_D_mixed_DRM"
+## example
+path_in <- "/projects/rpci/lsuchest/lsuchest/DBMT_results/DBMT_mixed/analyses/mixed_EA_results/out"
+path_out <- "/projects/rpci/lsuchest/lsuchest/DBMT_results/DBprep/out"
+patt <- "2_D_mixed_DRM"
 
-message("path_in:", path_in)
-message("path_out: ", path_out)
-message("patt: ", patt)
+message("\npath_in:", path_in, "\n")
+message("\npath_out: ", path_out, "\n")
+message("\npatt: ", patt, "\n")
+
 
 res_file <- dir(path_in, glue::glue("^{patt}"), full.names = TRUE)
 
 # string manipulation for file names and outcome_str
 patt %>%
-  str_split(., "_") %>% unlist() -> outcm_splt
+  str_split(., "_") %>% .[[1]] -> outcm_splt
 
 save_file_as <- paste(
   outcm_splt[2],
@@ -31,7 +33,10 @@ save_file_as <- paste(
   ),
   sep = "_"
 )
+
+
 message("Output will be saved as ", save_file_as)
+
 
 # load file
 chr_res <- vroom::vroom(
@@ -53,18 +58,21 @@ chr_res <- vroom::vroom(
   ),
   progress = FALSE
 )
-
 message("Result file loaded.")
+
 
 # modify and save
 chr_res %>%
   mutate_at(vars(matches("PVALUE*")), list(nlog10 = ~ log10(.) * -1)) %>%
+  mutate(POS = as.integer(POS)) %>%
   unite(snp_str, CHR:TYPED) %>%
   select(-matches("PVALUE_*"),
-         ends_with("nlog10")) %>% 
+         ends_with("nlog10")) %>%
   write_tsv(path = paste(path_out, save_file_as, sep="/"))
-message("File ", save_file_as, " is saved")
+message(save_file_as, "______saved")
 
 
-system(glue::glue("scp {path_out}/{save_file_as} ezgikara@199.109.192.37:/data/DBMT_results_database/results_tbl/"))
+system(glue::glue("scp {path_out}/{save_file_as} ezgikara@199.109.192.37:/data/DBMT_results_database/results_tbl/mixed_1y_EA"))
 message("File transfer complete.")
+
+
